fc9b4d3c5ca8ce2cff4f97d2996f92371c391baa
Huzaifa Shakeel <59845371+huzaifashakeel@users.noreply.github.com>
2021-07-19 01:19:47 -0700

    Create screen_nav.dart
    file for navigation of screens

---
+++ lib/helpers/screen_nav.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ignore: non_constant_identifier_names
+void ChangeScreen(BuildContext context, Widget widget) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
+}
+
+// ignore: non_constant_identifier_names
+void ChangeScreenReplacement(BuildContext context, Widget widget) {
+  Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (context) => widget));
+}
