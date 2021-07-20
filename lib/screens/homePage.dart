c572115e96c7547049644ae5d21b637eb70eb5ee
Huzaifa Shakeel <59845371+huzaifashakeel@users.noreply.github.com>
2021-07-20 02:42:32 -0700

    home screen updated
    home screen updated

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,10 +1,22 @@
 import 'package:events_app/widgets/customtext.dart';
+import 'package:events_app/widgets/event_explore.dart';
+import 'package:events_app/widgets/event_feed.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class HomePage extends StatefulWidget {
-  //const HomePage({Key? key}) : super(key: key);
+  //for testing purpose only
   bool isfeed = false;
+  List<String> mylist = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg"
+  ];
+  //const HomePage({Key? key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -21,164 +33,114 @@ class _HomePageState extends State<HomePage> {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Center(
-                  child: CustomText(
-                text: "Events",
-                size: 30,
-                fontWeight: FontWeight.bold,
-                letterspacing: 5,
-              )),
-            ),
-            // Padding(padding: EdgeInsets.only(top: 10)),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Card(
-                    elevation: 2,
-                    // decoration: BoxDecoration(
-                    //   borderRadius: BorderRadius.all(Radius.circular(20)),
-                    //   boxShadow: [
-                    //     BoxShadow(
-                    //       color: Colors.grey,
-                    //       blurRadius: 2.0,
-                    //       spreadRadius: 0.0,
-                    //       offset: Offset(
-                    //           2.0, 2.0), // shadow direction: bottom right
-                    //     )
-                    //   ],
-                    // ),
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.search,
-                          size: 30,
-                        )),
-                  ),
-                  Padding(padding: EdgeInsets.only(left: width * 0.1)),
-                  Container(
-                    height: height * 0.07,
-                    width: width * 0.5,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade300,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Card(
-                          elevation: widget.isfeed ? 2 : 0,
-                          child: FlatButton(
-                              onPressed: () {}, child: Text("Explore")),
-                        ),
-                        FlatButton(onPressed: () {}, child: Text("Feed")),
-                      ],
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.only(left: width * 0.08)),
-                  Card(
-                    elevation: 2,
-                    // decoration: BoxDecoration(
-                    //   borderRadius: BorderRadius.all(Radius.circular(20)),
-                    //   boxShadow: [
-                    //     BoxShadow(
-                    //       color: Colors.grey,
-                    //       blurRadius: 2.0,
-                    //       spreadRadius: 0.0,
-                    //       offset: Offset(
-                    //           2.0, 2.0), // shadow direction: bottom right
-                    //     )
-                    //   ],
-                    // ),
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.filter_1,
-                          size: 30,
-                        )),
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Center(
+                    child: CustomText(
+                  text: "Events",
+                  size: 30,
+                  fontWeight: FontWeight.bold,
+                  letterspacing: 5,
+                )),
               ),
-            ),
-
-            Column(
-              children: [
-                Stack(
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
                   children: [
+                    Card(
+                      elevation: 2,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.search,
+                            size: 30,
+                          )),
+                    ),
+                    Padding(padding: EdgeInsets.only(left: width * 0.085)),
                     Container(
-                      height: height * 0.3,
-                      width: width * 0.45,
-                      child: Card(
-                        elevation: 2,
-                        child: Container(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image.asset(
-                              "images/5.jpg",
-                              fit: BoxFit.fill,
-                            ),
+                      height: height * 0.07,
+                      width: width * 0.5,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Card(
+                            color: widget.isfeed
+                                ? Colors.white
+                                : Colors.grey.shade300,
+                            elevation: widget.isfeed ? 2 : 0,
+                            child: FlatButton(
+                                autofocus: false,
+                                onPressed: () {
+                                  setState(() {
+                                    widget.isfeed = false;
+                                  });
+                                },
+                                child: Text("Explore")),
                           ),
-                        ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
+                          Card(
+                            elevation: widget.isfeed ? 0 : 2,
+                            color: widget.isfeed
+                                ? Colors.grey.shade300
+                                : Colors.white,
+                            child: FlatButton(
+                                onPressed: () {
+                                  setState(() {
+                                    widget.isfeed = true;
+                                  });
+                                },
+                                child: Text("Feed")),
+                          ),
+                        ],
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(12, 12, 0, 0),
-                      child: Container(
-                        height: height * 0.04,
-                        width: width * 0.15,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            color: Colors.black),
-                        child: Center(
-                          child: CustomText(
-                            text: "Online",
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            size: 15,
-                          ),
-                        ),
-                      ),
+                    Padding(padding: EdgeInsets.only(left: width * 0.1)),
+                    Card(
+                      elevation: 2,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.filter_1,
+                            size: 30,
+                          )),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(130, 135, 0, 0),
-                      child: Container(
-                        height: height * 0.1,
-                        width: width * 0.12,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            color: Colors.black),
-                        child: Center(
-                          child: CustomText(
-                            text: "July \n 06",
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            size: 15,
-                          ),
-                        ),
-                      ),
-                    )
                   ],
                 ),
-                CustomText(
-                  text: "Musical consert",
-                  fontWeight: FontWeight.bold,
-                ),
-                CustomText(
-                  text: "At UET Taxila CS Department",
-                  fontWeight: FontWeight.w500,
-                  size: 13,
-                ),
-              ],
-            )
-          ],
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Column(
+                    children: widget.mylist
+                        .map((item) => GestureDetector(
+                              onTap: () {},
+                              child: widget.isfeed
+                                  ? EventFeed(
+                                      image: item,
+                                    )
+                                  : EventExp(image: item),
+                            ))
+                        .toList()),
+              ),
+
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //   children: [
+              //     EventExp(image: "images/5.jpg"),
+              //     EventExp(
+              //       image: "images/4.jpg",
+              //     ),
+              //   ],
+              // )
+            ],
+          ),
         ),
       ),
     );
