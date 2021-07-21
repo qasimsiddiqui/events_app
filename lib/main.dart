181976a135560c3314e4c1478b24d4c0a3d25496
Huzaifa Shakeel <59845371+huzaifashakeel@users.noreply.github.com>
2021-07-21 09:45:13 -0700

    Initialized necessary provider in main
    Initialized necessary provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:events_app/providers/eventProvider.dart';
+import 'package:events_app/screens/create_event.dart';
 import 'package:events_app/screens/homePage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: EventProvider.initialize())
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +28,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.white,
         buttonColor: Colors.white,
       ),
-      home: HomePage(),
+      home: CreateEvent(),
     );
   }
 }
