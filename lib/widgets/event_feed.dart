1c8bdbf0c0d22ee4625d9b29cc34aec0f07019cf
qasimsiddiqui <qasim.siddiqui678@gmail.com>
2021-08-01 02:24:46 +0500

    feed widget revamped

--- lib/widgets/event_feed.dart
+++ lib/widgets/event_feed.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:events_app/widgets/customtext.dart';
 import 'package:flutter/material.dart';
 
@@ -19,82 +21,101 @@ class _EventFeedState extends State<EventFeed> {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Column(
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        elevation: 2,
+        borderRadius: BorderRadius.circular(10),
+        child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Stack(
               children: [
                 Container(
-                  height: height * 0.35,
-                  width: width * 0.8,
-                  child: Card(
-                    elevation: 2,
+                  height: _height * 0.35,
+                  width: _width * 0.95,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10), topLeft: Radius.circular(10)),
+                      image: DecorationImage(image: AssetImage(widget.image), fit: BoxFit.cover)),
+                ),
+                ClipRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 0.5, sigmaY: 0.5),
                     child: Container(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          widget.image,
-                          fit: BoxFit.fill,
-                        ),
+                      width: _width * 0.95,
+                      height: _height * 0.35,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.5),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10), topLeft: Radius.circular(10)),
                       ),
                     ),
-                    shape:
-                        RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
-                  height: height * 0.06,
-                  width: width * 0.2,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)), color: Colors.black),
-                  child: Center(
-                    child: CustomText(
-                      text: "Online",
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      size: 15,
-                    ),
-                  ),
-                ),
-                Padding(
-                  //get screen size and adjust pedding (later)
-                  padding: const EdgeInsets.fromLTRB(250, 150, 0, 0),
-                  child: Container(
-                    height: height * 0.12,
-                    width: width * 0.15,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20)), color: Colors.black),
-                    child: Center(
-                      child: CustomText(
-                        text: "July \n 06",
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        size: 15,
+                  height: _height * 0.35,
+                  width: _width * 0.95,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 5, 0, 0),
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Chip(
+                            label: Text(
+                              'Online',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            backgroundColor: Colors.black87,
+                            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          ),
+                        ),
                       ),
-                    ),
+                      Padding(
+                        //get screen size and adjust pedding (later)
+                        padding: const EdgeInsets.fromLTRB(0, 0, 5, 5),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Container(
+                            height: _height * 0.10,
+                            width: _width * 0.16,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(20)),
+                                color: Colors.black87),
+                            child: Center(
+                              child: CustomText(
+                                text: "July \n 06",
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                size: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 170, 0, 0),
+                Positioned(
+                  bottom: 10,
+                  left: 10,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       CustomText(
                         text: "Musical consert",
                         fontWeight: FontWeight.bold,
-                        size: 24,
+                        size: 17,
                         color: Colors.white,
                       ),
                       CustomText(
                         text: "At UET Taxila CS Department",
-                        fontWeight: FontWeight.w500,
-                        size: 15,
+                        fontWeight: FontWeight.normal,
+                        size: 12,
                         color: Colors.white,
                       ),
                     ],
@@ -102,62 +123,37 @@ class _EventFeedState extends State<EventFeed> {
                 )
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-              child: Row(
-                children: [
-                  CircleAvatar(radius: 25, backgroundImage: AssetImage("images/7.jpg")),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomText(
-                          text: 'Huzaifa Shakeel',
-                          size: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        CustomText(
-                          text: 'Comptech Head',
-                          size: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey.shade700,
-                        ),
-                      ],
-                    ),
+            ListTile(
+              leading: CircleAvatar(backgroundImage: AssetImage("images/7.jpg")),
+              title: Text(
+                'Huzaifa Shakeel',
+              ),
+              subtitle: Text(
+                'Comptech Head',
+              ),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    likes++;
+                  });
+                },
+                child: Container(
+                  width: 60,
+                  child: Row(
+                    children: [
+                      Icon(Icons.thumb_up),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("$likes"),
+                      )
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50),
-                    child: Card(
-                      color: Colors.grey.shade300,
-                      elevation: 2,
-                      child: Row(
-                        children: [
-                          IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  likes++;
-                                });
-                              },
-                              icon: Icon(Icons.thumb_up)),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("$likes"),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+                ),
               ),
             ),
-            Divider(
-              color: Colors.black,
-              thickness: 2,
-            )
           ],
         ),
-      ],
+      ),
     );
   }
 }
